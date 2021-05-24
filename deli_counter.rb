c31c3f67f4a9fb04bf9a9520ffad225c3e42f24d
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 17:59:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,11 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.size == 0
+    
   katz_deli.each do |name|
-    puts ""
+    puts "Currently serving #{name}."
+    katz_deli.shift
+  end
   puts "There is nobody waiting to be served!"
 end
