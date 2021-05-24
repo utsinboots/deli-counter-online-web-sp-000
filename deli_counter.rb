6a7b47c6ec5847199aecae5b04ba6d265266041b
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 18:09:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,11 +22,7 @@ def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
-    #katz_deli.each do |name|
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
-    #line(katz_deli)
-  #end
-  #now_serving(katz_deli)
 end
