68748a5ad23dde6e02a8ddcf0ab315e05c71c5d4
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 17:53:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,6 @@ end
 
 def take_a_number(katz_deli, name)
   #katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
 end
   
