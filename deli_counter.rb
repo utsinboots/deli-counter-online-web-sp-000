25a4c78acb3dfb3478183c4e3db3c040304c244a
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 18:10:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,4 @@
-# Write your code here.
 def line(customer)
-  #show everyone their current place in line
-  #if nobody in line, print "The line is currently empty."
   line = ""
   if customer.size == 0
     puts "The line is currently empty."
