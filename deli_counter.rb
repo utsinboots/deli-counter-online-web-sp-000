5bfd978b49209a36626b4d90e4a5762b8b6901ca
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 17:42:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,12 @@
 def line(customer)
   #show everyone their current place in line
   #if nobody in line, print "The line is currently empty."
-  line_array = []
+  line = ""
   if customer.size == 0
     puts "The line is currently empty."
   else
     customer.each_with_index do |customer_name, index|
-      line_array << (index + 1) + ". " + customer_name
+      line << (index + 1) + ". " + customer_name
     end
     puts "The line is currently: #{line}"
   end
